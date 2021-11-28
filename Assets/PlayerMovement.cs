15468dc3259e209304c4b1b41caed9516b5b67f4
Mhosam <36338254+Mhosam@users.noreply.github.com>
2021-11-28 17:49:39 +0200

    Updated Tilemap
    Updated Tilemaps with collision.
    Added Player animation.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,10 +8,12 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5.0f;
     Rigidbody2D rb;
     Vector2 movement;
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,6 +21,8 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        anim.SetFloat("Horizontal", -movement.x);
+        anim.SetFloat("Speed", movement.sqrMagnitude);
     }
     private void FixedUpdate()
     {
